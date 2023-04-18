6e2c195d4c137a9d0860ca21f15bc91aa817cbd1
Cao1224 <yuc094@ucsd.edu>
2023-04-17 22:27:42 -0700

    files

--- flexbodyapp/app/src/main/java/com/codepath/flexbody/SplashActivity.kt
+++ flexbodyapp/app/src/main/java/com/codepath/flexbody/SplashActivity.kt
@@ -4,19 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.codepath.flexbody.MainActivity
-import com.codepath.flexbody.R
+import android.os.Looper
 
 class SplashActivity  : AppCompatActivity() {
-    private val SPLASH_TIME_OUT = 3000L
+
+    var handler = Handler().postDelayed({
+        val i = Intent(this@SplashActivity, LoginActivity::class.java)
+        startActivity(i)
+    }, 1400)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, SPLASH_TIME_OUT)
     }
 }
