1abdcba5aedecde24d5af9f232cb73fabdb796c6
Melissa Perez <melmperz@gmail.com>
2023-04-24 20:59:06 -0400

    Add logout button

--- flexbodyapp/app/src/main/java/com/codepath/flexbody/fragments/HomeFragment.kt
+++ flexbodyapp/app/src/main/java/com/codepath/flexbody/fragments/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.codepath.flexbody.R
+import com.parse.ParseUser
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +19,14 @@ class HomeFragment : Fragment() {
         activity?.title = "Home"
 
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        view.findViewById<Button>(R.id.logOutButton).setOnClickListener {
+            ParseUser.logOut()
+            activity?.finish()
+        }
     }
 
     companion object {
