01706ceae99a06679667f56dad16e6485d06911a
emmanuel.fanibi <58971180+efanibi25@users.noreply.github.com>
2023-04-25 18:49:42 -0500

    merge conflicts

--- flexbodyapp/app/src/main/java/com/codepath/flexbody/MainActivity.kt
+++ flexbodyapp/app/src/main/java/com/codepath/flexbody/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.codepath.flexbody
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         val personalFragment: Fragment = PersonalFragment()
         val feedFragment: Fragment = FeedFragment()
 
+
         binding?.bottomNavigation?.setOnItemSelectedListener { item ->
             lateinit var fragment: Fragment
             when (item.itemId) {
@@ -45,6 +50,19 @@ class MainActivity : AppCompatActivity() {
 
         binding?.bottomNavigation?.selectedItemId = R.id.action_home
         Log.d(TAG, "MainActivity onCreate() method called")
+
+//        if (Intent.ACTION_SEARCH == intent.action) {
+//            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+//                val bundle = Bundle()
+//                bundle.putString(query, "From Activity")
+//// set Fragmentclass Arguments
+//// set Fragmentclass Arguments
+//                val fragobj = Fragmentclass()
+//                fragobj.setArguments(bundle)
+//
+//
+//            }
+//        }
 
     }
 
---
+++ flexbodyapp/app/src/main/java/com/codepath/flexbody/Nutrition.kt
@@ -0,0 +1,5 @@
+package com.codepath.flexbody
+
+class Nutrition(
+    val name: String) {
+}
